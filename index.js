a39aac1beef9f0c8d05a8808d1f156e64cf35f9a
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-04-01 10:15:41 +0200

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+// 1. Concatenation.
+// Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+
+console.log(introSentence + " "+ age);
+console.log(introSentence.concat(age));
+
+
+// 2. Converting
+// Create a variable with the value of "1005". Convert it to a number.
+// Create a variable with the value of "10.05". Convert it to a number.
+let number1 = "1005";
+console.log(parseInt(number1));
+
+let number2 = "10.05";
+console.log(parseFloat(number2));
